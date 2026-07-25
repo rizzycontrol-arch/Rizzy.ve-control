368dba983ec78a89ac168d920e71aa2466a544fa
rizzycontrol-arch <rizzycontrol@gmail.com>
2026-07-25 11:30:49 -0400

    Respaldo + mejoras: login, producto mas vendido, comision 5%

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+env: {
+NEXT_PUBLIC_SUPABASE_URL: "https://htsrwpmgbryhqfyfbczx.supabase.co",
+NEXT_PUBLIC_SUPABASE_ANON_KEY:
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh0c3J3cG1nYnJ5aHFmeWZiY3p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ5MTM3NjYsImV4cCI6MjEwMDQ4OTc2Nn0.7ETmMZu4wg1W3bQAgeMFrqrfq1PtsQEDlh2-TmRPPQk",
+},
+};
+export default nextConfig;
